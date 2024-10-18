6a175d2bf15c01773467a19dc57775aa915f958b
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-18 12:51:29 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/LineageW/20220726/bg_boss.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
